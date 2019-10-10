d9387f346cad733463646bfe4184594ad4ab286c
rywjhzd <rywang@strw.leidenuniv.nl>
2019-10-11 00:58:57 +0200

    Add subviews in disk app

--- diskcatalog/disk/views.py
+++ diskcatalog/disk/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 #from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1>This is the disk app homepage.<h1>")
+    return HttpResponse("<h1>This is the disk app homepage.</h1>")
+
+def detail(request, resolved_id):
+    return HttpResponse("<h2>Details for resolved disks: " + str(resolved_id) + "</h2>")
--- diskcatalog/diskcatalog/urls.py
+++ diskcatalog/diskcatalog/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('disk', include('disk.urls')),
+    path('disk/', include('disk.urls')),
 ]
