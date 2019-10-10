aa648643618b98864b06a3da8dda7676a227cc5d
rywjhzd <rywang@strw.leidenuniv.nl>
2019-10-10 22:17:43 +0200

    Update models.py

--- diskcatalog/disk/models.py
+++ diskcatalog/disk/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Resolved(models.Model):
+    object = models.CharField(max_length=30)
+    category = models.CharField(max_length=10)
+    spectype = models.CharField(max_length=10)
+    Rband = models.CharField(max_length=10)
+    distance = models.CharField(max_length=10)
+    diskmajoraxis = models.CharField(max_length=10)
+    diskdiameter = models.CharField(max_length=10)
+    inclination = models.CharField(max_length=10)
