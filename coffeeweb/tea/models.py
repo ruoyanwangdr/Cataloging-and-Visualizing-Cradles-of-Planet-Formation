de4cac255090a07340c44beed97ab74e7c3381c7
rywjhzd <rywang@strw.leidenuniv.nl>
2019-10-10 15:20:20 +0200

    Create database

--- coffeeweb/tea/models.py
+++ coffeeweb/tea/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Teaa(models.Model):
+    name = models.CharField(max_length=100)
+    taste = models.CharField(max_length=200)
+    country = models.CharField(max_length=50)
+    image = models.CharField(max_length=500)
+
+class Coffee(models.Model):
+    coffeename = models.ForeignKey(Teaa, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10)
+    title = models.CharField(max_length=100)
