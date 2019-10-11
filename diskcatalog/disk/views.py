e9fccef1167e54a4c0a6dc43b598320ac2e2dbfb
rywjhzd <rywang@strw.leidenuniv.nl>
2019-10-11 16:03:46 +0200

    Clean up using render shortcut

--- diskcatalog/disk/views.py
+++ diskcatalog/disk/views.py
@@ -1,20 +1,12 @@
 from django.http import HttpResponse
-from django.template import loader
 from .models import Resolved
-#from django.shortcuts import render
+from django.shortcuts import render
 
 def index(request):
     all_resolved = Resolved.objects.all()
-    template = loader.get_template('disk/index.html')
-    context = {
-        'all_resolved' : all_resolved,
-    }
-    return HttpResponse(template.render(context, request))
-    #html = ''
-    #for disks in all_resolved:
-    #    url = '/disk/' + str(disks.id) + '/'
-    #    html += '<a href="' + url + '">' + disks.object + '</a><br>'
-    #return HttpResponse(html)
+    context = {'all_resolved' : all_resolved}
+    return render(request, 'disk/index.html', context)
+
 
 def detail(request, resolved_id):
     return HttpResponse("<h2>Details for resolved disks: " + str(resolved_id) + "</h2>")
