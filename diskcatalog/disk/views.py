a82d76a1b32050a4d66e18f835579ce867669be0
rywjhzd <rywang@strw.leidenuniv.nl>
2019-10-11 20:45:08 +0200

    Raise a 404 HTTP error

--- diskcatalog/disk/views.py
+++ diskcatalog/disk/views.py
@@ -1,12 +1,16 @@
-from django.http import HttpResponse
+from django.http import Http404
 from .models import Resolved
 from django.shortcuts import render
 
+
 def index(request):
     all_resolved = Resolved.objects.all()
-    context = {'all_resolved' : all_resolved}
-    return render(request, 'disk/index.html', context)
+    return render(request, 'disk/index.html', {'all_resolved' : all_resolved})
 
 
 def detail(request, resolved_id):
-    return HttpResponse("<h2>Details for resolved disks: " + str(resolved_id) + "</h2>")
+    try:
+        resolved = Resolved.objects.get(pk=resolved_id)
+    except Resolved.DoesNotExist:
+        raise Http404("Disk does not exist.")
+    return render(request, 'disk/detail.html', {'resolved' : resolved})
