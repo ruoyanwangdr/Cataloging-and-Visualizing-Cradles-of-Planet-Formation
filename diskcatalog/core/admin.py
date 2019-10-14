cd60764d69422af82eefb38ea6dc5a68033b1869
rywjhzd <rywang@strw.leidenuniv.nl>
2019-10-14 21:45:48 +0200

    Add a missing column and display in web

--- diskcatalog/core/admin.py
+++ diskcatalog/core/admin.py
@@ -3,6 +3,6 @@ from .models import Disk
 
 admin.site.register(Disk)
 class DiskAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'category', 'distance', 'r_band', 'disk_major_axis', 'disk_diameter', 'inclination', 'resolution_elements_across', 'at_ref_wavelength')
-    search_fields = ('object', 'category', 'distance', 'r_band', 'disk_major_axis', 'disk_diameter', 'inclination', 'resolution_elements_across', 'at_ref_wavelength')
+    list_display = ('__str__', 'category', 'spec_type', 'r_band', 'distance', 'disk_major_axis', 'disk_diameter', 'inclination', 'resolution_elements_across', 'at_ref_wavelength')
+    search_fields = ('object', 'category', 'spec_type', 'r_band', 'distance', 'disk_major_axis', 'disk_diameter', 'inclination', 'resolution_elements_across', 'at_ref_wavelength')
     list_filter = ('catalog',)
--- diskcatalog/core/models.py
+++ diskcatalog/core/models.py
@@ -3,13 +3,14 @@ from django.db import models
 class Disk(models.Model):
     object = models.CharField('object', max_length=100)
     category = models.CharField('category', max_length=100)
-    r_band = models.CharField('r_band', max_length=10)
-    distance = models.CharField('distance', max_length=10)
-    disk_major_axis = models.CharField('disk_major_axis', max_length=10)
-    disk_diameter = models.CharField('disk_diameter', max_length=10)
-    inclination = models.CharField('inclination', max_length=10)
-    resolution_elements_across = models.CharField('resolution_elements_across', max_length=10)
-    at_ref_wavelength = models.CharField('at_ref_wavelength', max_length=10)
+    spec_type = models.CharField('spec_type', max_length=100)
+    r_band = models.CharField('r_band', max_length=100)
+    distance = models.CharField('distance', max_length=100)
+    disk_major_axis = models.CharField('disk_major_axis', max_length=100)
+    disk_diameter = models.CharField('disk_diameter', max_length=100)
+    inclination = models.CharField('inclination', max_length=100)
+    resolution_elements_across = models.CharField('resolution_elements_across', max_length=100)
+    at_ref_wavelength = models.CharField('at_ref_wavelength', max_length=100)
     #references = models.CharField('references', max_length=10)
     if_resolved =(
         ('Resolved', 'Resolved'),
