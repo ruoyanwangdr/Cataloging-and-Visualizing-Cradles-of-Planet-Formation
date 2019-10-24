74e9d7389337836d54514287aed71649b1abb43f
rywjhzd <rywang@strw.leidenuniv.nl>
2019-10-24 15:12:53 +0200

    Update views.py

--- diskcatalog/core/views.py
+++ diskcatalog/core/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .models import Disk
+import os
+
 
 def index(request):
     context = {}
     disk_list = Disk.objects.all()
     context['disk_list'] = disk_list
     return render(request, 'index.html', context)
+
+
+#def index(request):
+#    module_dir = os.path.dirname(__file__)
+#    file_path = os.path.join(module_dir, 'data.txt')
+#    disk_list = open(file_path , 'r')
+#    data = data_file.read()
+#    context = {'disk_list': data}
+#    return render(request, 'index.html', context)
