86f1eeb4022c581a94518f985784eb0a867402e3
rywjhzd <rywang@strw.leidenuniv.nl>
2019-11-10 13:38:46 +0000

    Recreate database; Adjust primitive positions

--- diskcatalog/core/models.py
+++ diskcatalog/core/models.py
@@ -3,15 +3,17 @@ from django.db import models
 class Disk(models.Model):
     object = models.CharField('object', max_length=100)
     category = models.CharField('category', max_length=100, null=True, blank=True)
-    spec_type = models.CharField('spec_type', max_length=100, null=True, blank=True)
-    r_band = models.CharField('r_band', max_length=100, null=True, blank=True)
+    ra = models.CharField('ra', max_length=100, null=True, blank=True)
+    dec = models.CharField('dec', max_length=100, null=True, blank=True)
     distance = models.CharField('distance', max_length=100, null=True, blank=True)
+    system_age = models.CharField('system_age', max_length=100, null=True, blank=True)
+    central_star_mass = models.CharField('central_star_mass', max_length=100, null=True, blank=True)
     disk_major_axis = models.CharField('disk_major_axis', max_length=100, null=True, blank=True)
-    disk_diameter = models.CharField('disk_diameter', max_length=100, null=True, blank=True)
+    #disk_diameter = models.CharField('disk_diameter', max_length=100, null=True, blank=True)
     inclination = models.CharField('inclination', max_length=100, null=True, blank=True)
-    resolution_elements_across = models.CharField('resolution_elements_across', max_length=100, null=True, blank=True)
-    at_ref_wavelength = models.CharField('at_ref_wavelength', max_length=100, null=True, blank=True)
-    #references = models.CharField('references', max_length=10, null=True, blank=True)
+    #resolution_elements_across = models.CharField('resolution_elements_across', max_length=100, null=True, blank=True)
+    #at_ref_wavelength = models.CharField('at_ref_wavelength', max_length=100, null=True, blank=True)
+    references = models.CharField('references', max_length=10, null=True, blank=True)
     if_resolved =(
         ('Resolved', 'Resolved'),
         ('Unresolved', 'Unresolved'),
