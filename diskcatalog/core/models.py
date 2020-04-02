333f58ddda7988cd143a9c50c43c7a82974ef97e
rywjhzd <rywang@strw.leidenuniv.nl>
2020-04-02 16:46:24 +0200

    Update models.py

--- diskcatalog/core/models.py
+++ diskcatalog/core/models.py
@@ -3,15 +3,16 @@ from django.db import models
 class Disk(models.Model):
     object = models.CharField(max_length=100)
     category = models.CharField(max_length=100, null=True, blank=True)
-    ra = models.CharField(max_length=100, null=True, blank=True)
-    dec = models.CharField(max_length=100, null=True, blank=True)
+    spec_type = models.CharField(max_length=100, null=True, blank=True)
+    r_band = models.CharField(max_length=100, null=True, blank=True)
     distance = models.CharField(max_length=100, null=True, blank=True)
-    system_age = models.CharField(max_length=100, null=True, blank=True)
-    stellar_mass = models.CharField(max_length=100, null=True, blank=True)
     disk_major_axis = models.CharField(max_length=100, null=True, blank=True)
     inclination = models.CharField(max_length=100, null=True, blank=True)
+    ra = models.CharField(max_length=100, null=True, blank=True)
+    dec = models.CharField(max_length=100, null=True, blank=True)
+    system_age = models.CharField(max_length=100, null=True, blank=True)
+    stellar_mass = models.CharField(max_length=100, null=True, blank=True)
     references = models.CharField(max_length=1000, null=True, blank=True)
-    catalog = models.CharField(max_length=100, null=True, blank=True)
 
     class Meta:
         ordering = ('object',)
